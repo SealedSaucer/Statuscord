d6c32b45184fa5b6cf7fabc02a201c3e4af92f8e
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2021-08-12 10:56:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client } = require('discord.js-selfbot-v11')
 const client = new Client();
 
 client.on('ready', () => { 
-  client.user.setActivity("Subscribe!", {type: "STREAMING", url: "https://twitch.tv/ninja"})
+  client.user.setActivity("Subscribe!", {type: "STREAMING", url: "https://twitch.tv/SealedSaucer"})
    console.log(`${client.user.username} Successfully Logged in!`)
 })
 
