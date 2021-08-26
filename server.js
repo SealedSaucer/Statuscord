16ad7956c6ce37ba90cc78aac2554bb210b765d9
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2021-08-26 14:49:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send('<meta http-equiv="refresh" content="0; URL=https://phantomcodes.ga/credits"/>')
+  res.send('<meta http-equiv="refresh" content="0; URL=https://phantomcodes.ga/support"/>')
 })
 
 function keepAlive() {
