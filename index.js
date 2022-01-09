4164190e6a63af52ee648b0067f71b8723ad6483
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-01-09 14:35:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const keepAlive = require("./server")
+let discord = require('discord.js-selfbot-v11')
+let rpcGenerator = require("discordrpcgenerator")
 const dotenv = require('dotenv');
 const TOKEN = (process.env.TOKEN);
-const { Client } = require('discord.js-selfbot-v11')
-const client = new Client();
-
-client.on('ready', () => { 
-  client.user.setActivity("Subscribe!", {type: "STREAMING", url: "https://twitch.tv/SealedSaucer"})
-   console.log(`${client.user.username} Successfully Logged in!`)
-})
+var uuid = ()=>([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,a=>(a^Math.random()*16>>a/4).toString(16))
+let client = new discord.Client()
 
 keepAlive()
-client.login(TOKEN);
+client.login(TOKEN)
+ 
+client.on("ready", () => {
+    rpcGenerator.getRpcImage("CLIENT_ID", "IMAGE_NAME")
+    .then(image => {
+        let presence = new rpcGenerator.Rpc()
+        .setName("twitch")
+        .setUrl("https://twitch.tv/SealedSaucer")
+        .setType("STREAMING")
+        .setApplicationId("CLIENT_ID")
+        .setAssetsLargeImage(image.id)
+        .setAssetsLargeText("youtube.com/SealedSaucer")
+        .setDetails("Subscribe!")
+ 
+        client.user.setPresence(presence.toDiscord())
+    }).catch(console.error)
+  console.log(`${client.user.username} Successfully Logged in!`)
+})
