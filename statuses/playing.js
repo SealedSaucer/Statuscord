9e6036e2fc57817254177cd16ce0e990a546dd2e
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-11-24 18:42:58 +0530

    Update playing.js

--- statuses/playing.js
+++ statuses/playing.js
@@ -1,4 +1,4 @@
-// Change this
-const GAME_NAME = "The name of the game";
+// Change this to the name of the game you want
+const GAME_NAME = "Cyberpunk 2077";
 
 module.exports = (client) => client.user.setActivity(GAME_NAME, { type: "PLAYING" });
