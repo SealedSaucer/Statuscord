7dd1c2563ffbb51db0d958ae270226c3db763b57
code913 <54856929+code913@users.noreply.github.com>
2022-11-24 11:45:11 +0000

    refactor: :art: make everything better, time to test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+// code913 was here :)
+
 const
   express = require("express"),
   chalk = require("chalk"),
   server = express(),
   prompt = require("prompt-sync")({sigint: true}),
   statuses = new Map([
-    [1, "playing"],
-    [2, "listening"],
-    [3, "streaming"]
+    [1, ["playing", chalk.yellowBright.bold]],
+    [2, ["listening", chalk.greenBright.bold]],
+    [3, ["streaming", chalk.magentaBright.bold]]
   ]);
 
 console.log(`${chalk.cyanBright.bold("Statuscord")} | ${chalk.greenBright.bold("SealedSaucer")}`);
@@ -19,17 +21,22 @@ console.log(`[${chalk.green.bold("+")}] The webserver is ready.`);
 console.log(
   `[${chalk.yellow.bold("!")}] Which presence would you like to start?`,
   [ ...statuses.entries() ]
-  .map((number, statusName) => `[${number}] ${statusName.replace(/^./, m => m.toUpperCase())}`)
+  .map((number, [statusName]) => `[${number}] ${statusName.replace(/^./, m => m.toUpperCase())}`)
   .join("\n")
 );
 const number = prompt("> ");
-const statusName = statuses.get(+number);
+const [statusName, style] = statuses.get(+number);
 
 if (statusName) {
   const statusModule = require(`./statuses/${statusName}.js`);
 
   console.clear();
-  statusModule(client);
+  // Underscore argument ignore syntax inspired by Elixir
+  client.on("ready", _ => statusModule(client)
+    .then(_ => console.log(`[${style(statusName.toUpperCase())}] Successfully logged in as ${client.user.username} (${client.user.id})!`))
+    .catch(console.error)
+  );
 } else {
   console.log(`[${chalk.red.bold("-")}] Invalid option.`);
+  process.exit();
 }
